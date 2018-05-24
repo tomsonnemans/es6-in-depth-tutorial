3297cb4e783eee2c1ea990e7cbc4285c4de7dfd3
Tom Sonnemans <tom@madia.nl>
2018-05-24 13:51:20 +0200

    prototypes

---
+++ app/index.js
@@ -0,0 +1,17 @@
+function Wizard(name, house, pet) {
+    this.name = name;
+    this.house = house;
+    this.pet = pet;
+    this.greet = () => `I'm ${this.name} from ${this.house}`;
+}
+
+Wizard.prototype.pet_name = 'Hedwig'
+
+Wizard.prototype.info = function() {
+    return `I have a ${this.pet} named ${this.pet_name}.`;
+};
+
+let harry = new Wizard('Harry', 'Gryffindor', 'Owl');
+harry.pet_name = 'Hedwig';
+
+console.log(harry.info());
