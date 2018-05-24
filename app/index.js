6c1b42fa7f428e8bc4446cb49926e7ad97fad80d
Tom Sonnemans <tom@madia.nl>
2018-05-24 14:59:50 +0200

    private methods

---
+++ app/index.js
@@ -0,0 +1,22 @@
+const budget = () => {
+    let balance = 0;
+
+    let changeBal = (val) => {
+        return balance += val;
+    }
+
+    const deposit20 = () => changeBal(20);
+    const withdraw20 = () => changeBal(-20);
+    const check = () => balance;
+
+    return { deposit20, withdraw20, check }
+}
+
+let wallet = budget();
+
+wallet.deposit20();
+wallet.withdraw20();
+wallet.deposit20();
+wallet.deposit20();
+
+console.log(wallet.check());
