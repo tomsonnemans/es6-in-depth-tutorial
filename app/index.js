9105589e447799d431cab84340321159eea6edc0
Tom Sonnemans <tom@madia.nl>
2018-05-24 15:22:22 +0200

    add itterator

--- app/index.js
+++ app/index.js
@@ -1,3 +1,49 @@
+// const arrayItterator = (array) => {
+//     let index = 0;
+
+//     return {
+//         next: () => {
+//             if( index < array.length ) {
+//                 let next = array[index];
+//                 index += 1;
+//                 return next;
+//             }
+//         }
+//     }
+// }
+
+// let it = arrayItterator([1, 2, 3]);
+
+function* arrayItterator() {
+    yield* arguments;
+}
+
+let it = arrayItterator(1, 2, 3);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+
+function* evens() {
+    let count = 0;
+    while(true) {
+        count += 2;
+        let reset = yield count;
+        if(reset) {
+            count = 0;
+        }
+    }
+}
+
+let sequence = evens();
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next(true).value);
+console.log(sequence.next().value);
+
+
+
 function* countMaker() {
     let count = 0;
     while (count < 6) {
@@ -6,9 +52,3 @@ function* countMaker() {
 }
 
 let countGen = countMaker();
-console.log(countGen.next().value);
-console.log(countGen.next().value);
-console.log(countGen.next().value);
-console.log(countGen.next().value);
-console.log(countGen.next().value);
-console.log(countGen.next().value);
