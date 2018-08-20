1af4cb933e1460a5464683750842942e559c9a33
Tom Sonnemans <tom@madia.nl>
2018-08-20 09:16:09 +0200

    basics

---
+++ app/index.js
@@ -0,0 +1,8 @@
+let p = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Resolved promise data'), 3000);
+})
+
+p.then(response => console.log(response))
+ .catch(error => console.log(error));
+
+console.log('after the promise');
